58bd12a4aad6fdffe04c7b09b97e58888a9115e7
Nicholas <nick.saravia@gmail.com>
2023-07-18 15:54:09 -0400

    create user in planetscale db using clerkId

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,3 +1,30 @@
-export default function Page() {
-  return <div>new-user</div>;
+import { prisma } from '@/utils/db';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+const createNewUser = async () => {
+  const user = await currentUser();
+  if (!user) throw new Error('No user found');
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+
+  redirect('/journal');
+};
+
+export default async function Page() {
+  await createNewUser();
+  return <div>loading...</div>;
 }
